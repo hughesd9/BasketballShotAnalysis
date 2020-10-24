2b1532b6a9e3737f182202ef8e11c491483889d6
David Hughes <hughesd9@tcd.ie>
2020-10-24 15:42:37 +0100

    Imported pandas and plotly. Read the CSV file using pandas and created a basic scatter plot using plotly.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-Hello World!
+import pandas as pd
+import plotly.express as px
+
+grouped_shots_df = pd.read_csv('srcdata/league_shots_by_dist.csv')
+
+fig = px.scatter(grouped_shots_df, x="avg_dist", y="shots_accuracy")
+fig.show()
