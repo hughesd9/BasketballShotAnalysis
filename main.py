10484243d4f5852dfc7a7ef85cf2a4756a1ab7a4
David Hughes <hughesd9@tcd.ie>
2020-10-24 15:51:52 +0100

    Added bubbles to scatter to show the amount of shots taken at a particular distance as well as colours for different shot types.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import plotly.express as px
+import pickle
 
 grouped_shots_df = pd.read_csv('srcdata/league_shots_by_dist.csv')
 
-fig = px.scatter(grouped_shots_df, x="avg_dist", y="shots_accuracy")
+fig = px.scatter(grouped_shots_df, x="avg_dist", y="shots_accuracy", size="shots_counts", color='shot_type', size_max=25)
 fig.show()
