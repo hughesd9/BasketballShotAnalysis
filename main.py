76dcbfd9416a0b0fd26926180a94d8464cd00fd2
David Hughes <hughesd9@tcd.ie>
2020-10-26 17:14:00 +0000

    Added import for numpy and began working on draw_plotly_court to draw the court on the hexbin visualization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 import pickle
 
 #grouped_shots_df = pd.read_csv('srcdata/league_shots_by_dist.csv')
@@ -23,3 +24,32 @@ fig.add_trace(go.Scatter(x=xlocs, y=ylocs, mode='markers', name='markers', marke
 )))
 
 fig.show(config=dict(displayModeBar=False))
+
+#definition to create court layout
+
+def draw_plotly_court(fig, fig_width=600, margins=10):
+
+    # From: https://community.plot.ly/t/arc-shape-with-path/7205/5
+    def ellipse_arc(x_center=0.0, y_center=0.0, a=10.5, b=10.5, start_angle=0.0, end_angle=2 * np.pi, N=200, closed=False):
+        t = np.linspace(start_angle, end_angle, N)
+        x = x_center + a * np.cos(t)
+        y = y_center + b * np.sin(t)
+        path = f'M {x[0]}, {y[0]}'
+        for k in range(1, len(t)):
+            path += f'L{x[k]}, {y[k]}'
+        if closed:
+            path += ' Z'
+        return path
+
+    fig_height = fig_width * (470 + 2 * margins) / (500 + 2 * margins)
+    fig.update_layout(width=fig_width, height=fig_height)
+
+    #set axes ranges
+    fig.update_xaxes(range=[-250 - margins, 250 + margins])
+    fig.update_yaxes(range=[-52.5 - margins, 417.5 + margins])
+
+    threept_break_y = 89.47765084
+    three_line_col = "#777777"
+    main_line_col = "#777777"
+
+
